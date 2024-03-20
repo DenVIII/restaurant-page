bd53cd6e461c4404577ae5890aaec22b91b6a95b
DenVIII <Denis226@gmail.com>
2024-03-20 13:40:08 +0300

    Change dish titles and descriptions

--- src/pages/menu-positions.js
+++ src/pages/menu-positions.js
@@ -1,17 +1,17 @@
 const menuData = [
     {
-        title: 'Dish-1',
-        text: 'Lorem ipsum',
-        price: '250g/100g 40$'
+        title: 'L’inde au pâté',
+        text: 'An exquisite piece of turkey with pate and spaghetti is the perfect dish for any occasion. Tender turkey meat goes well with a fragrant pate, and a side dish of spaghetti adds satiety to the dish. Try this delicious dish and you will not remain indifferent!',
+        price: '250g/150g/100g 40$'
     },
     {
-        title: 'Dish-2',
-        text: 'Lorem ipsum',
-        price: '150g 50$'
+        title: 'Fruit Salad',
+        text: 'Fruit salad is an ideal choice for those who want to enjoy the taste of summer and at the same time not overload their stomach. Perfect for a hot day and will cheer you up!',
+        price: '200g 30$'
     },
     {
-        title: 'Dish-3',
-        text: 'Lorem ipsum',
+        title: 'Chef\'s special',
+        text: 'The pastry chef offers a classic dessert made from fresh ingredients. It is a table decoration with a unique taste. Treat yourself and your loved ones with a sweet masterpiece from the chef!',
         price: '200g/50g/50g 45$'
     }
 ]
